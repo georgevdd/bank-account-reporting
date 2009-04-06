24b0a8937ee1a5f6eb84d3fc45373c503af634fb
gbmvdd <gbmvdd@a747239e-1d7a-11de-bf1e-7b5f7763e386>
2009-04-06 18:43:53 +0000

    - Added Database, Schema modules, and a script to import all statements. - Added BeautifulSoup just for good measure.

---
+++ ImportStatements.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import sys
+import Model
+from Database import ensureSession, resetSession
+from MsMoney import Statement
+import StatementStore
+
+def importAll():
+    session = ensureSession()
+    for filename in StatementStore.genDownloadedStatementFilenames():
+        stmt = Statement(open(filename))
+        print '%s - %s' % (stmt.beginDate, stmt.endDate),
+        stmt_exists = (session.query(Model.ImportedStatement)
+                  .filter_by(beginDate=stmt.beginDate, endDate=stmt.endDate)
+                  .first() is not None)
+        for trn in stmt.transactions:
+            trn_exists = (session.query(Model.Transaction)
+                  .filter_by(fitid=trn.fitid)
+                  .first() is not None)
+            if trn_exists ^ stmt_exists:
+                raise Exception('Something very odd about fitid %s' % trn.fitid)
+            if not trn_exists:
+                # Create a Model.Transaction out of a
+                # MsMoney.Transaction.
+                trn = Model.Transaction(trn.date, trn.amount,
+                    trn.fitid, trn.name, trn.memo)
+                session.add(trn)
+        if stmt_exists:
+            print 'already exists.'
+        else:
+            session.add(Model.ImportedStatement(stmt.beginDate, stmt.endDate))
+            print 'saved.'
+            
+    resetSession()
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        StatementStore.STORE_PATH = sys.argv[1]
+    print 'Looking for statements in', StatementStore.STORE_PATH
+    importAll()
