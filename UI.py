215ccd9a8eba2b41050fc7408e2e0c26de917482
gbmvdd <gbmvdd@a747239e-1d7a-11de-bf1e-7b5f7763e386>
2009-12-09 23:19:50 +0000

    Added a bunch of functionality to the statements window, including display of invoiceability/paymentness and a menu command to select all likely payments.

--- UI.py
+++ UI.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 from datetime import date
-from GUI import Window, Table, application
+from GUI import Window, Menu, Table, application
 import Model as M
-import Queries
+import Queries as Q
+import Database as D
 
 def invoiceableSymbol(t):
   if t.isInvoiceable:
@@ -21,34 +22,51 @@ def paymentSymbol(t):
   else:
     return ''
 
-def showStatements():
-  columns = [
-    ("Day", lambda t: t.date.day),
-    ("i?", invoiceableSymbol),
-    ("p?", paymentSymbol),
-    ("Amount", lambda t: float(t.amount)),
-    ("Memo", lambda t: str(t.memo)),
-    ]
-  transactions_tbl = Table(columns = columns, scrolling='hv')
-  
-  rows = Queries.allTransactionMonths()
-  columns = [
-    ("Month", lambda (y, m): date(y, m, 1).strftime("%Y %b")),
-    ]
-  months_tbl = Table(rows=rows, columns=columns, scrolling='v')
+class StatementsWindow(Window):
+  def __init__(self):
+    columns = [
+      ("Day", lambda t: t.date.day),
+      ("i?", invoiceableSymbol),
+      ("p?", paymentSymbol),
+      ("Amount", lambda t: float(t.amount)),
+      ("Memo", lambda t: str(t.memo)),
+      ]
+    self.transactions_tbl = Table(columns = columns, scrolling='hv',
+                                  multi_select=True)
 
-  def month_changed():
-    y, m = rows[months_tbl.selected_row]
-    transactions_tbl.rows = Queries.transactionsForMonth(y, m)
-  months_tbl.selection_changed_action = month_changed
+    columns = [
+      ("Month", lambda (y, m): date(y, m, 1).strftime("%Y %b")),
+      ]
+    rows = D.withSession(Q.allTransactionMonths)
+    self.months_tbl = Table(rows=rows, columns=columns, scrolling='v',
+                            selection_changed_action = self.month_changed)
 
-  win = Window(title = "Statements")
-  win.size = (500, 500)
-  win.place(months_tbl, left=0, right=100, top=0, bottom=0, sticky='nsw')
-  win.place(transactions_tbl,
-            left=months_tbl, right=0, top=0, bottom=0, sticky='nesw')
-  win.show()
+    Window.__init__(self, title="Statements", size=(1000, 500))
+    self.place(self.months_tbl, left=0, right=100, top=0, bottom=0, sticky='nsw')
+    self.place(self.transactions_tbl,
+              left=self.months_tbl, right=0, top=0, bottom=0, sticky='nesw')
+    self.menus = [
+      Menu("Statements", [
+          ("Select likely payments/L", "select_likely_payments"),
+           ])
+           ]
 
-showStatements()
+  def setup_menus(self, m):
+    m.select_likely_payments.enabled = bool(self.transactions_tbl.rows)
+
+  def month_changed(self):
+    y, m = self.months_tbl.rows[self.months_tbl.selected_row]
+    self.transactions_tbl.rows = D.withSession(lambda: Q.transactionsForMonth(y, m))
+    self.transactions_tbl.selected_rows = []
+
+  def select_likely_payments(self):
+    selection = []
+    for i, r in enumerate(self.transactions_tbl.rows):
+      if r.isLikelyInvoiceable:
+        selection.append(i)
+    self.transactions_tbl.selected_rows = selection
+
+win = StatementsWindow()
+win.show()
 
 application().run()
