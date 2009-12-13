a98351ffe9425acaaad723aa0167d0cbfb20d963
gbmvdd <gbmvdd@a747239e-1d7a-11de-bf1e-7b5f7763e386>
2009-12-13 19:38:29 +0000

    Added UI for marking a transaction as a payment.

--- UI.py
+++ UI.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 from datetime import date
-from GUI import Window, Menu, Table, application
+from GUI import Window, ModalDialog, Menu, Table, application
 import Model as M
 import Queries as Q
 import Database as D
+
+import AppKit
+AppKit.NSAnyEventMask = 0xffffffff
 
 def invoiceableSymbol(t):
   if t.isInvoiceable:
@@ -22,6 +25,23 @@ def paymentSymbol(t):
   else:
     return ''
 
+class PersonPicker(ModalDialog):
+  def __init__(self):
+    ModalDialog.__init__(self, resizable=True)
+    self.persons_tbl = Table(columns=[('fullName', lambda p: p.fullName)],
+                             scrolling='v',
+                             selection_changed_action=self.person_picked)
+    self.place(self.persons_tbl, left=0, right=0, top=0, bottom=0, sticky='nesw')
+
+  def pick(self):
+    session = D.requireSession()
+    self.persons_tbl.rows = session.query(M.Person).all()
+    return self.present()
+
+  def person_picked(self):
+    selected_person = self.persons_tbl.rows[self.persons_tbl.selected_rows.next()]
+    self.dismiss(result=selected_person)
+
 class StatementsWindow(Window):
   def __init__(self):
     columns = [
@@ -31,7 +51,7 @@ class StatementsWindow(Window):
       ("Amount", lambda t: float(t.amount)),
       ("Memo", lambda t: str(t.memo)),
       ]
-    self.transactions_tbl = Table(columns = columns, scrolling='hv',
+    self.transactions_tbl = Table(columns=columns, scrolling='hv',
                                   multi_select=True)
 
     columns = [
@@ -39,7 +59,7 @@ class StatementsWindow(Window):
       ]
     rows = D.withSession(Q.allTransactionMonths)
     self.months_tbl = Table(rows=rows, columns=columns, scrolling='v',
-                            selection_changed_action = self.month_changed)
+                            selection_changed_action=self.month_changed)
 
     Window.__init__(self, title="Statements", size=(1000, 500))
     self.place(self.months_tbl, left=0, right=100, top=0, bottom=0, sticky='nsw')
@@ -49,12 +69,14 @@ class StatementsWindow(Window):
       Menu("Statements", [
           ("Select likely invoiceables/L", "select_likely_invoiceables"),
           ("Mark selection as invoiceable/I", "mark_selection_as_invoiceable"),
+          ("Mark selection as payment/P", "mark_selection_as_payment"),
            ]),
            ]
 
   def setup_menus(self, m):
     m.select_likely_invoiceables.enabled = bool(self.transactions_tbl.rows)
-    m.mark_selection_as_invoiceable.enabled = bool(list(self.transactions_tbl.selected_rows))
+    m.mark_selection_as_invoiceable.enabled = bool(self.transactions_tbl.selected_row)
+    m.mark_selection_as_payment.enabled = len(list(self.transactions_tbl.selected_rows)) == 1
 
   def month_changed(self):
     y, m = self.months_tbl.rows[self.months_tbl.selected_row]
@@ -76,7 +98,15 @@ class StatementsWindow(Window):
     D.withSession(do_mark)
     self.transactions_tbl._ns_inner_view.reloadData()
 
-win = StatementsWindow()
-win.show()
+  def mark_selection_as_payment(self):
+    def do_mark():
+      t = self.transactions_tbl.rows[self.transactions_tbl.selected_row]
+      person = PersonPicker().pick()
+      if person:
+        Q.markTransactionAsPayment(t, person)
+    D.withSession(do_mark)
+    
 
-application().run()
+if __name__ == '__main__':
+  win = StatementsWindow().show()
+  application().run()
