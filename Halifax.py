1764c7cc36aa3f97dce76a98f43b43f2e5b83bfe
gbmvdd@gmail.com <gbmvdd@gmail.com@a747239e-1d7a-11de-bf1e-7b5f7763e386>
2010-11-22 00:58:38 +0000

    Updated Halifax.py to cope with new Memorable Information login step.

--- Halifax.py
+++ Halifax.py
@@ -1,6 +1,5 @@
-from mechanize import urlopen, Request
+from mechanize import urlopen, Request, ParseResponse, ParseFile
 from urllib import urlencode
-from ClientForm import ParseResponse, ParseFile
 from StringIO import StringIO
 from BeautifulSoup import BeautifulSoup
 from sys import stdin, stderr
@@ -12,6 +11,7 @@ docsListHit = False
 
 HOMEPAGE_URL = 'http://www.halifax-online.co.uk'
 HOMEPAGE_TITLE = 'Halifax - Welcome to Online'
+MEMORABLE_INFO_FORM_TITLE = 'Enter memorable information'
 DOCS_LIST_URL = ('https://online-documents.halifax-online.co.uk' +
     '/onlinedocuments/app/oddocuments.asp')
 DOC_URL = ('https://online-documents.halifax-online.co.uk'+
@@ -19,19 +19,29 @@ DOC_URL = ('https://online-documents.halifax-online.co.uk'+
 LOGOUT_URL = 'https://www.halifax-online.co.uk/_mem_bin/SignOff.asp'
 
 class LoginInfo(object):
-    def __init__(self, username, password, securityAnswer):
+    def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.securityAnswer = securityAnswer
 
+def getLoginInfo():
+    return LoginInfo(getpass('Username: '), getpass())
 
-def getLoginInfo(question):
-    return LoginInfo(getpass('Username: '), getpass(), getpass(question + ' '))
+def getMemorableInfo(indices):
+    return getpass('Memorable info characters %d, %d and %d: ' % tuple(indices))
 
 def checkPageTitle(soup, expectedTitle):
-    title = soup.title.contents[0]
+    title = soup.title.contents[0].strip()
     if expectedTitle != title:
+        global errorSoup
+        errorSoup = soup
         raise Exception('Title was "%s", not "%s"' % (title, expectedTitle))
+
+def checkForErrors(soup):
+    errors = soup.findAll(id='AdditionalInfo')
+    if errors:
+        global errorSoup
+        errorSoup = soup
+        raise Exception(errors)
 
 def doWithRetries(func, retries = 4, **kwargs):
     while (retries > 0):
@@ -42,44 +52,73 @@ def doWithRetries(func, retries = 4, **kwargs):
         retries -= 1
     return func(**kwargs)
 
-def getLoginForm():
-    response = urlopen(HOMEPAGE_URL)
-    responseText = response.read()
-    
-    soup = BeautifulSoup(responseText)
-    checkPageTitle(soup, HOMEPAGE_TITLE)
-    
-    prompts = soup.findAll(name='td', attrs={'class':'bwLoginMCUser'})
-    formSoup = soup.find("form", {"id" : "frmFormsLogin"})
-    question = formSoup.find("input", {"name" : "answer", "class" : "LoginAnswer"})["alt"]
-    if not question.endswith('?'):
-        raise Exception('Found "%s" while trying to find question.')
-    
-    forms = ParseFile(StringIO(responseText), response.geturl(), 
-            backwards_compat=False)
-    form = forms[1]
-    
-    return form, question
+class LoginForm(object):
+    def __init__(self, httpResponse):
+        responseText = httpResponse.read()
+        soup = BeautifulSoup(responseText)
 
-def postLoginForm(form):
-    homepageText = urlopen(form.click('btnContinue')).read()
-    soup = BeautifulSoup(homepageText)
-    errors = soup.findAll(attrs={'color':'red'})
-    if errors:
-        raise Exception(errors)
+        checkPageTitle(soup, HOMEPAGE_TITLE)
+
+        forms = ParseFile(StringIO(responseText),
+                          httpResponse.geturl(),
+                          backwards_compat=False)
+        self.form = forms[1]
+
+    def populate(self, loginInfo):
+        self.form['Username'] = loginInfo.username
+        self.form['password'] = loginInfo.password
+        self.form.find_control('JSEnabled').readonly = False
+        self.form['JSEnabled'] = 'true'
+
+    def submissionRequest(self):
+        return self.form.click('btnContinue')
+
+class MemorableInfoForm(object):
+    def __init__(self, httpResponse):
+        pageText = httpResponse.read()
+        soup = BeautifulSoup(pageText)
+
+        checkForErrors(soup)
+        checkPageTitle(soup, MEMORABLE_INFO_FORM_TITLE)
+
+        self.memorableInfoIndices = [
+            int(soup
+                .find('span', id='ctl00_MainPageContent_labelChar%d' % i)
+                .string[-1])
+            for i in range(1, 4)]
+
+        forms = ParseFile(StringIO(pageText),
+                          httpResponse.geturl(),
+                          backwards_compat=False)
+        self.form = forms[0]
+
+    def populate(self, memorableInfoCharacters):
+        if len(memorableInfoCharacters) != 3:
+            raise ValueError('Expected three memorable info characters.')
+
+        for i, v in zip(range(1, 4), memorableInfoCharacters.lower()):
+            self.form.set_value_by_label(
+                ['\xa0%s\xa0' % v],
+                ('ctl00$MainPageContent$char%d$DropDownList' % i))
+        self.form.find_control('ctl00$javascriptEnabled1').readonly = False
+        self.form['ctl00$javascriptEnabled1'] = 'true'
+
+    def submissionRequest(self):
+        return self.form.click('ctl00$MainPageContent$_signin')
 
 def logIn():
-    form, question = doWithRetries(getLoginForm)
-    
-    loginInfo = getLoginInfo(question)
+    loginForm = LoginForm(urlopen(HOMEPAGE_URL))
+    loginInfo = getLoginInfo()
+    loginForm.populate(loginInfo)
 
-    form.find_control('JSEnabled').readonly = False
-    form['JSEnabled'] = 'true'
-    form['Username'] = loginInfo.username
-    form['password'] = loginInfo.password
-    form['answer'] = loginInfo.securityAnswer
+    memorableInfoForm = MemorableInfoForm(urlopen(
+        loginForm.submissionRequest()))
+    memorableInfo = getMemorableInfo(memorableInfoForm.memorableInfoIndices)
+    memorableInfoForm.populate(memorableInfo)
 
-    postLoginForm(form)
+    pageText = urlopen(memorableInfoForm.submissionRequest()).read()
+    checkForErrors(BeautifulSoup(pageText))
+
     global loggedIn, docsListHit
     loggedIn = True
     docsListHit = False
