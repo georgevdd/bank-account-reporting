cddf5ec780eca5d20754edd456415b1fe3f2aebf
gbmvdd@gmail.com <gbmvdd@gmail.com@a747239e-1d7a-11de-bf1e-7b5f7763e386>
2011-12-28 11:27:54 +0000

    A small script to give an idea of what dependencies are required.

---
+++ install_dependencies.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+sudo easy_install mechanize
+curl -o ~/postgresql-8.3.5-2-osx.dmg http://downloads.enterprisedb.com/postgresql/postgresql-8.3.5-2-osx.dmg
+open postgresql-8.3.5-2-osx.dmg
+open /Volumes/PostgreSQL\ 8.3.5-2/postgresql-8.3.5-2-osx.app/
